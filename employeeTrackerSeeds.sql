860ca74fd01d07bf079d1561d7c0bef226aa09a5
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-22 17:04:04 -0400

    inserting data in the table

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -24,8 +24,16 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NULL
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES roles (id),
     FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
+
+INSERT INTO departments (name) VALUES ("Development"), ("Marketing");
+
+INSERT INTO roles (title, salary, department_id) VALUES ("Engineering Team Lead", 130000, 1), ("Software Engineer", 100000, 1);
+
+INSERT INTO roles (title, salary, department_id) VALUES ("Digital Strategist", 85000, 2), ("Social Media Manager", 50000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Marissa", "Gray", 1, 1), ("James", "Frances", 2, null), ("Daniel", "Bruce", 3, 1), ("Kelly", "Davis", 4, null);
