23eacaabf8ab6b2c4328aea3a88aa71600a77cd5
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-22 15:52:06 -0400

    created employee table

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -18,3 +18,14 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES departments (id)
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
