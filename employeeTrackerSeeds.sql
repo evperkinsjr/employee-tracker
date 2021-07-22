456931c185626217d399f3f558edf17643f1896f
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-22 15:36:18 -0400

    created roles table

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -9,3 +9,12 @@ CREATE TABLE departments (
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments (id)
+);
