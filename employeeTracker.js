e2f75f5e9cf3e334cb27f100ad56d908e22f6630
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-25 18:26:23 -0400

    pseudo-coding

--- employeeTracker.js
+++ employeeTracker.js
@@ -3,3 +3,41 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 require('dotenv').config();
+
+// Create connection for SQL database
+
+
+// Prompt user for the action they should take
+
+
+// View Departments
+
+
+// View Roles
+
+
+// View Employees
+
+
+// Add Departments
+
+
+// Add Roles
+
+
+// Add Employees
+
+
+// Update Employee Roles
+
+
+// Update Employee Managers
+
+
+// Delete Departments
+
+
+// Delete Roles
+
+
+// Delete Employees
