f26ce0a0ef057de1244578561254c293d008bf85
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-25 18:35:52 -0400

    created connection for sql db

--- employeeTracker.js
+++ employeeTracker.js
@@ -5,7 +5,13 @@ const consoleTable = require('console.table');
 require('dotenv').config();
 
 // Create connection for SQL database
-
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 // Prompt user for the action they should take
 
